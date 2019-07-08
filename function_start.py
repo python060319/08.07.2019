9a4a2b9c40b1088d28c994fdcefe12af6faee395
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-08 18:11:18 +0300

    Create function_start.py

---
+++ function_start.py
@@ -0,0 +1,38 @@
+
+
+# functions !!!
+# whenever you see <command>( )
+# build our own functions
+#   prevent duplication of code
+#   sharing of abilities
+#   modularity
+#   ability
+
+print('hello')
+# no hoisting in python
+
+def printMyName():
+    pass
+
+def print1To10():
+    pass
+
+# this does not run !!!!!!!!
+def printHelloWorld():
+    print('hello world')
+
+printHelloWorld()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
